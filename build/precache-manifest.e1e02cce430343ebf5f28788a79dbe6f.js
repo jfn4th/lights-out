0fc500c90d632bb384d5c6c6311238a0e49867ed
Jamie Nelson <jfn4th@gmail.com>
2020-05-19 23:16:48 -0500

    github pages build

---
+++ build/precache-manifest.e1e02cce430343ebf5f28788a79dbe6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4706227e115bc570d35e0b7ea9a5dcd",
+    "url": "/lights-out/index.html"
+  },
+  {
+    "revision": "2ccf1c0082077e3e7a04",
+    "url": "/lights-out/static/css/main.e0e62c91.chunk.css"
+  },
+  {
+    "revision": "5516b575f16c8489fdeb",
+    "url": "/lights-out/static/js/2.9e7a55ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lights-out/static/js/2.9e7a55ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ccf1c0082077e3e7a04",
+    "url": "/lights-out/static/js/main.d9473e38.chunk.js"
+  },
+  {
+    "revision": "ec2cc572359196689239",
+    "url": "/lights-out/static/js/runtime-main.af0af72b.js"
+  }
+]);
